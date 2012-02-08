b02b5e9bd00ae72642e4c5e397f773097f4a8214
msauvette <martialsauvette@gmail.com>
2012-02-08 19:22:48 +0100

    first step to productiv test

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#wget http://sourceforge.net/projects/jboss/files/JBoss/JBoss-4.2.3.GA/jboss-4.2.3.GA-jdk6.zip/download >jboss.zip
+
+wget http://sourceforge.net/projects/jboss/files/JBoss/JBoss-5.1.0.GA/jboss-5.1.0.GA-jdk6.zip/download >jboss.zip
+
+unzip jboss.zip
+export JBOSS_HOME=
+
+
+
+wget http://sourceforge.net/projects/jboss/files/JBoss%20Seam/2.2.2.Final/jboss-seam-2.2.2.Final.zip/download
+unzip jboss.zip
+
+
+
+https://github.com/martailsauvette/tagsobe-jboss-seam-hotel-booking/zipball/master
